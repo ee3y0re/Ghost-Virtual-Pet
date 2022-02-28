5be3c17048c4308e8ba4e02d08f3bce09b1001b9
ajoyvmon <ajoyvmon@gmail.com>
2022-02-27 22:27:14 -0800

    ghost is alive

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import {Ghost} from './scripts/ghost';
 document.addEventListener('DOMContentLoaded', () => {
 
   //background and game
-  const canvas = document.getElementById("play");
-  const ctx = canvas.getContext("2d");
-  ctx.canvas.width = 1000; //size of canvas and play area
-  ctx.canvas.height = 562; //size of canvas and play area
+  // const canvas = document.getElementById("play");
+  // const ctx = canvas.getContext("2d");
+  // ctx.canvas.width = 1000; //size of canvas and play area
+  // ctx.canvas.height = 562; //size of canvas and play area
 
   /* 
   ghost
@@ -23,9 +23,9 @@ document.addEventListener('DOMContentLoaded', () => {
   // console.log(test)
   // test.full()
   // console.log(test)
-  const ghost = new Image(); //initialized presence of ghost
-  ghost.src = "../assets/ghost-Sheet-master-blazter-big.png" //linked image ghost to source in assets folder
-  ghost.onload = () => { //begin the drawing
-    ctx.drawImage(ghost, 500, 300); //image and position
-  }
+  // const ghost = new Image(); //initialized presence of ghost
+  // ghost.src = "../assets/ghost-Sheet-master-blazter-big.png" //linked image ghost to source in assets folder
+  // ghost.onload = () => { //begin the drawing
+  //   ctx.drawImage(ghost, 500, 300); //image and position
+  // }
 });
