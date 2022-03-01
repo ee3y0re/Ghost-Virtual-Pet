ee794bd77c30fbe40b8a2be20eba50dbdc538cb5
ajoyvmon <ajoyvmon@gmail.com>
2022-02-28 17:29:26 -0800

    hungry cycle

--- src/index.js
+++ src/index.js
@@ -3,52 +3,40 @@ import {Ghost} from './scripts/ghost';
 document.addEventListener('DOMContentLoaded', () => {
   const sassper = new Ghost();
 
-  // // // setTimeout Practice
   function getHungry() {
-    console.log('in async function')
     return new Promise(sayHunger => {
       setTimeout(() => {
+        console.log("in getHungry")
         sassper.hungry();
         const hungerTag = document.getElementById("hunger")
         if (hungerTag.innerText === "") {
           hungerTag.innerText = "That disgusting thing hanging off your nose reminds me of boo-gers. Speaking of which, I'm hungry!";
         }
-      }, 10000);
+        sassper.testSatiety();
+      }, 3000);
     });
   }
 
   async function callHungry() {
-    console.log("starting")
+    console.log("in callHungry")
     const speak = await getHungry();
     console.log(speak)
   }
 
-  callHungry();
+  //not sure if i need this line
+  let hungerID;
 
-  // // setInterval Practice
-  // let hungerID;
-
-  // function hungryV2() {
-  //   console.log("start")
-  //   if (!hungerID) {
-  //     hungerID = setInterval(getHungry2, 5000);
-  //   }
-  // }
-
-  // function getHungry2(){
-  //   console.log("running hunger")
-  //   const hungryElm = document.getElementsByClassName("hunger")
-  //   if (hungryElm === "") {
-  //     hungryElm = "That disgusting thing hanging off your nose reminds me of boo-gers.<br> Speaking of which, I'm hungry!";
-  //   } else {
-  //     hungryElm === "";
-  //   }
-  // }
+  function hungryCycle() {
+    console.log("in hungryCycle")
+    if (!hungerID) {
+      hungerID = setInterval(callHungry, 30000);
+    }
+  }
 
   // function notHungryAnymore() {
   //   clearInterval(hungerID);
   //   hungerID = null;
   // }
 
-  // hungryV2();
+  hungryCycle();
 });
