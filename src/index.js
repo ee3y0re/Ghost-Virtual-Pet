661d0f17a5ce61de126c5570811fc1b603264f13
ajoyvmon <ajoyvmon@gmail.com>
2022-02-28 16:02:10 -0800

    adjusted some more styles and started forming async functions

--- src/index.js
+++ src/index.js
@@ -1,31 +1,47 @@
 import {Ghost} from './scripts/ghost';
 
-//this is the security thingy delete after
-// window.Ghost = Ghost;
-
 document.addEventListener('DOMContentLoaded', () => {
+  const sassper = new Ghost();
 
-  //background and game
-  // const canvas = document.getElementById("play");
-  // const ctx = canvas.getContext("2d");
-  // ctx.canvas.width = 1000; //size of canvas and play area
-  // ctx.canvas.height = 562; //size of canvas and play area
-
-  /* 
-  ghost
-  the properties are changing
-  but for some reason, the object method isn't being defined on console
-  const test = new Ghost() 
-  */
-
-  // console.log(test)
-  // test.hungry()
-  // console.log(test)
-  // test.full()
-  // console.log(test)
-  // const ghost = new Image(); //initialized presence of ghost
-  // ghost.src = "../assets/ghost-Sheet-master-blazter-big.png" //linked image ghost to source in assets folder
-  // ghost.onload = () => { //begin the drawing
-  //   ctx.drawImage(ghost, 500, 300); //image and position
+  // function getHungry() {
+  //   return new Promise(sayHunger => {
+  //     setTimeout(() => {
+  //       sassper.hungry();
+  //       sayHunger() //grab html element to show this on screen
+  //     }, 10000);
+  //   });
   // }
+
+  // async function callHungry() {
+  //   const speak = await getHungry();
+  //   console.log(speak)
+  // }
+
+  // callHungry();
+
+  let repeatHunger;
+
+  function hungryV2() {
+    console.log("start")
+    if (!repeatHunger) {
+      repeatHunger = setInterval(getHungry2, 5000);
+    }
+  }
+
+  function getHungry2(){
+    console.log("running hunger code")
+    const hungryElm = document.getElementsByClassName("hunger")
+    if (hungryElm === "") {
+      hungryElm = "That disgusting thing hanging off your nose reminds me of boo-gers.<br> Speaking of which, I'm hungry!";
+    } else {
+      hungryElm === "";
+    }
+  }
+
+  function notHungryAnymore() {
+    clearInterval(repeatHunger);
+    repeatHunger = null;
+  }
+
+  hungryV2();
 });
--- src/scripts/ghost.js
+++ src/scripts/ghost.js
@@ -1,44 +1,38 @@
 export {Ghost}
 
 class Ghost {
-  // // setting default start stats
   constructor() {
-    // // level of fullness opposed to hunger
     this.satiety = 10; 
-    // // true means no presence of star poop
     this.hygiene = true; 
-    // // level meter, affection button is unlocked when ghost is full and clean
-    // // disabled for a moment after user uses
-    // // max lvl 3 for ghost to come to life
     this.affection = 0; 
-    // if true, level meter is able to go up
-    // if falselevel meter goes down by 1
-
   }
 
-  hungry(){
+  hungry() {
     this.satiety -= 1;
   }
-  full(){
+  full() {
     this.satiety += 1;
   }
 
-  dirty(){
+  dirty() {
     this.hygiene -= 1;
   }
-  clean(){
+  clean() {
     this.hygiene += 1;
   }
 
   //i think this will be async
-  receiveAffection(){
+  receiveAffection() {
     this.affection += 1;
   }
 
-  burning(){
+  burning() {
     this.affection -= 1;
   }
 
-  // I think i will do the text or levels as a separate class
-  // Next todo: export and import ghost and find on console
+  //test methods
+
+  testSatiety() {
+    console.log(this.satiety);
+  }
 }
